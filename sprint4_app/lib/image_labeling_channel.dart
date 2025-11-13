d352bd05b1baf9bf20c887f4b2a6f0a0c9a06b4e
Arthur Lopes <arthur.lopes@kobe.io>
2025-11-13 09:42:58 -0300

    chore: clean up comment lines on ImageLabelingChannel class

--- sprint4_app/lib/image_labeling_channel.dart
+++ sprint4_app/lib/image_labeling_channel.dart
@@ -3,14 +3,6 @@ import 'package:flutter/services.dart';
 class ImageLabelingChannel {
   static const _channel = MethodChannel('com.sprint4/image_labeling');
 
-  // static Future<List<String>> labelImage(String imagePath) async {
-  //   final List<dynamic> labels = await _channel.invokeMethod('labelImage', {
-  //     'path': imagePath,
-  //   });
-
-  //   return labels.cast<String>();
-  // }
-
   static Future<List<String>> labelImage(String assetPath) async {
     final bytes = await rootBundle.load(assetPath);
     final list = bytes.buffer.asUint8List();
