2d0e136cfca661465b8095968f3ac64b58556c22
Arthur Lopes <arthurpsl99@gmail.com>
2025-11-23 16:00:18 -0300

    feat: implement google sign in methods

--- sprint4_app/lib/common/service/sign_in/google_sign_in_service.dart
+++ sprint4_app/lib/common/service/sign_in/google_sign_in_service.dart
@@ -1,8 +1,25 @@
+import 'dart:async';
+
+import 'package:google_sign_in/google_sign_in.dart';
 import 'package:sprint4_app/common/service/sign_in/sign_in_protocol.dart';
 
 class GoogleSignInService implements SignInProtocol {
   @override
-  Future<String?> getIdToken({required String rawNonce}) async {
-    return await null;
+  Future<String?> getIdToken({String? rawNonce}) async {
+    const webClientId =
+        '72772928328-ftfj73lh337k2e3g5u50gn9qrpfo436q.apps.googleusercontent.com';
+
+    const iosClientId =
+        '72772928328-7ku5bpb4s8t5m89ose46fsqcstp9mqhr.apps.googleusercontent.com';
+
+    final GoogleSignIn signIn = GoogleSignIn.instance;
+
+    unawaited(
+      signIn.initialize(clientId: iosClientId, serverClientId: webClientId),
+    );
+
+    final googleAccount = await signIn.authenticate();
+    final googleAuthentication = googleAccount.authentication;
+    return googleAuthentication.idToken;
   }
 }
