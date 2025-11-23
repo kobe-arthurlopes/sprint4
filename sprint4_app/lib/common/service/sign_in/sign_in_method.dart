539598e11dbf746356b7906834b91b29fb771d27
Arthur Lopes <arthurpsl99@gmail.com>
2025-11-23 15:58:11 -0300

    refactor: add auth provider do sign in method enum

--- sprint4_app/lib/common/service/sign_in/sign_in_method.dart
+++ sprint4_app/lib/common/service/sign_in/sign_in_method.dart
@@ -1,6 +1,7 @@
 import 'package:sprint4_app/common/service/sign_in/apple_sign_in_service.dart';
 import 'package:sprint4_app/common/service/sign_in/google_sign_in_service.dart';
 import 'package:sprint4_app/common/service/sign_in/sign_in_protocol.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 enum SignInMethod {
   apple,
@@ -15,6 +16,17 @@ enum SignInMethod {
         return GoogleSignInService();
       case SignInMethod.unknown:
         return GoogleSignInService();
+    }
+  }
+
+  OAuthProvider oAuthProvider() {
+    switch (this) {
+      case SignInMethod.apple:
+        return OAuthProvider.apple;
+      case SignInMethod.google:
+        return OAuthProvider.google;
+      case SignInMethod.unknown:
+        return OAuthProvider.google;
     }
   }
 }
