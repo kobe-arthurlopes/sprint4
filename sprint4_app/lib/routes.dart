a2b32328402726095b2d7917577ef0705fcc6925
Arthur Lopes <arthur.lopes@kobe.io>
2025-11-26 12:36:38 -0300

    refactor: wrap routes config method inside a class

--- sprint4_app/lib/routes.dart
+++ sprint4_app/lib/routes.dart
@@ -5,23 +5,28 @@ import 'package:sprint4_app/common/service/supabase/supabase_service_protocol.da
 import 'package:sprint4_app/home/presentation/pages/home_page.dart';
 import 'package:sprint4_app/login/presentation/pages/login_page.dart';
 
-// Configuração do GoRouter
-GoRouter router(BuildContext context) {
-  final SupabaseServiceProtocol supabaseService = context.read();
-  String initialScreen = '/login';
-  if (supabaseService.authentication.isAuthenticated) {
-    initialScreen = '/';
+class Routes {
+  // Configuração do GoRouter
+  GoRouter config(BuildContext context) {
+    final SupabaseServiceProtocol supabaseService = context.read<SupabaseServiceProtocol>();
+    String initialScreen = '/login';
+
+    if (supabaseService.authentication.isAuthenticated) {
+      initialScreen = HomePage.routeId;
+    }
+
+    return GoRouter(
+      initialLocation: initialScreen,
+      routes: [
+        GoRoute(
+          path: HomePage.routeId,
+          builder: (_, _) => HomePage()   
+        ),
+        GoRoute(
+          path: LoginPage.routeId,
+          builder: (_, _) => LoginPage()
+        )
+      ]
+    );
   }
-  return GoRouter(
-    initialLocation: initialScreen,
-    routes: [
-      GoRoute(
-        path: '/',
-        builder: (context, state) => HomePage(viewModel: context.read())   
-      ),
-      GoRoute(path: '/login',
-      builder: (context, state) => LoginPage(viewModel: context.read())
-      )
-    ]
-  );
 }
